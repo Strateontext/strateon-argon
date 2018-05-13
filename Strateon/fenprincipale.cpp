567012ec7016b0135caa82fba6f393c846c5d793
Strateontext <39223380+Strateontext@users.noreply.github.com>
2018-05-13 15:31:28 +0200

    Update fenprincipale.cpp

--- Strateon/fenprincipale.cpp
+++ Strateon/fenprincipale.cpp
@@ -1,33 +1,46 @@
 #include "FenPrincipale.h"
-#include <QMainWindow>
-#include <QtGui>
-#include <QMenu>
-#include <QMenuBar>
+#include "mafenetre.h"
+
+
+
+void NouvelleFen() {
+    QTextEdit *zoneTexte1 = new QTextEdit;    // ...
+
+}
+
 
 
 FenPrincipale::FenPrincipale()
 {
 
+
     //Création des pages de texte
     QMdiArea *zoneCentrale = new QMdiArea;
     QTextEdit *zoneTexte1 = new QTextEdit;
-    QTextEdit *zoneTexte2 = new QTextEdit;
     QMdiSubWindow *sousFenetre1 = zoneCentrale->addSubWindow(zoneTexte1);
-    QMdiSubWindow *sousFenetre2 = zoneCentrale->addSubWindow(zoneTexte2);
+
 
 
     //Création du menu
     QMenu *a = menuBar()->addMenu("Fichier");
 
+    QAction *actionQuitter = a->addAction("&Quitter");
+    actionQuitter->setShortcut(QKeySequence("Ctrl+Q"));
+    a->addAction(actionQuitter);
+
+
+    connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+
         QMenu *Nouveau = a->addMenu("Nouveau");
+            Nouveau->addAction("Projet");
+            Nouveau->addAction("Fichier");
         QAction *Enregistrer = a->addAction("Enregistrer");
         QMenu *Enregistrersous = a->addMenu("Enregistrer sous...");
         QMenu *Ouvrir = a->addMenu("Ouvrir...");
         QMenu *Récent = a->addMenu("Récent");
         QMenu *Partager = a->addMenu("Partager");
         QMenu *Fermer = a->addMenu("Fermer");
-    Nouveau->addAction("Projet");
-    Nouveau->addAction("Fichier");
 
 
     //-------------------------------------------------------------------------//
@@ -44,12 +57,13 @@ FenPrincipale::FenPrincipale()
 
     //Création de la toolbar
     QToolBar *toolBarFichier = addToolBar("Fichier");
-    toolBarFichier->addAction("Affichage");
-    toolBarFichier->addSeparator();
+    toolBarFichier->addAction(actionQuitter);
     setUnifiedTitleAndToolBarOnMac(true);
-
-
-
+    QFontComboBox *choixPolice = new QFontComboBox;
+    toolBarFichier->addWidget(choixPolice);
     setCentralWidget(zoneCentrale);
+
 }
+
+
 
