d7117a670194c222cf281581a491458fd3d701bc
Strateontext <39223380+Strateontext@users.noreply.github.com>
2018-06-20 15:55:27 +0200

    Add files via upload

--- WindowsFormsApplication10/WindowsFormsApplication10/Form1.Designer.cs
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.Designer.cs
@@ -31,17 +31,25 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(286, 289);
+            this.button1.Location = new System.Drawing.Point(161, 260);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(70, 70);
             this.button1.TabIndex = 0;
@@ -52,28 +60,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(121, 28);
+            this.label1.Location = new System.Drawing.Point(121, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "...";
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(-471, -196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(8000, 10000);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 23);
             this.label2.Name = "label2";
@@ -88,7 +86,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(468, 25);
             this.label3.Name = "label3";
@@ -99,9 +97,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(577, 28);
+            this.label4.Location = new System.Drawing.Point(577, 25);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 37);
             this.label4.TabIndex = 5;
@@ -109,7 +107,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(479, 80);
+            this.button3.Location = new System.Drawing.Point(506, 91);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(141, 36);
             this.button3.TabIndex = 6;
@@ -117,21 +115,83 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(572, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Easy";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(572, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Medium";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(572, 191);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Hard";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Location = new System.Drawing.Point(452, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 68);
+            this.panel2.TabIndex = 11;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button7.Location = new System.Drawing.Point(1146, -267);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(8000, 10000);
+            this.button7.TabIndex = 2;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 68);
+            this.panel1.TabIndex = 12;          
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 549);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Clicker Game -- A Game of Click";
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,12 +201,18 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WindowsFormsApplication10/WindowsFormsApplication10/Form1.cs
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.cs
@@ -15,89 +15,100 @@ namespace WindowsFormsApplication10
         public Form1()
         {
             InitializeComponent();
-            
 
+            MessageBox.Show("Click on start to begin a game", "Welcome !");
+            level = 0;
         }
 
-
-
-        int count = 0;
-
-        int nb = 1;
+            int count = 0;
+            int nb = 1;
+            int level;
         private void button1_Click(object sender, EventArgs e)
         {
-
-            timer1.Start();
-            count = 0;
-            Random rX = new Random();
-            System.Threading.Thread.Sleep(1);
-            Random rY = new Random();
-            int X = rX.Next(10, 1800);
-            int Y = rY.Next(10, 960);
-
-
-            button1.Location = new System.Drawing.Point(X, Y);
-
-            nb += 1;
-            this.label1.Text = nb.ToString();
-
+            if (level == 0)
+            {
+                MessageBox.Show("Chose your level difficulty", "Level difficulty");
+                
+            }
+            else
+            {
+                
+                Random rb = new Random();
+                System.Threading.Thread.Sleep(1);
+                Random gb = new Random();
+                System.Threading.Thread.Sleep(1);
+                Random bb = new Random();
+                    int R = rb.Next(0, 255);
+                    int G = gb.Next(0, 255);
+                    int B = bb.Next(0, 255);
+                Color rgbabutton = Color.FromArgb(R, G, B);
+                button1.BackColor = rgbabutton;
+                timer1.Start();
+                timer2.Start();
+                count = 0;
+                Random rX = new Random();
+                System.Threading.Thread.Sleep(1);
+                Random rY = new Random();
+                    int X = rX.Next(10, 1800);
+                    int Y = rY.Next(10, 960);
+                button1.Location = new System.Drawing.Point(X, Y);
+                nb += 1;
+                this.label1.Text = nb.ToString();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
             nb -= 1;
             this.label1.Text = nb.ToString();
-
             Random rX2 = new Random();
             System.Threading.Thread.Sleep(1);
             Random rY2 = new Random();
-            int X2 = rX2.Next(10, 1800);
-            int Y2 = rY2.Next(10, 960);
-
-
+                int X2 = rX2.Next(10, 1800);
+                int Y2 = rY2.Next(10, 960);
             button1.Location = new System.Drawing.Point(X2, Y2);
 
         }
-
-        
+     
         private void timer1_Tick(object sender, EventArgs e)
         {
             count++;
             this.label4.Text = count.ToString();
-
-
-
-            if (count == 15)
+            if (count == level)
             {
-                button1.Hide();
-                button2.Hide();
-                MessageBox.Show("Temps écoulé !" + "votre score :" + nb, "Timer messagebox");
+                button1.Hide();               
+                MessageBox.Show("Time is up !" + "    Your score :" + nb, "Timer messagebox");
                 timer1.Stop();
-
-
+                level = 0;
                 count = 0;
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
-
+            if (level == 0)
+            {
+                MessageBox.Show("Chose your level difficulty", "Level difficulty");
+            }         
             count = 0;
             nb = 0;
             this.label1.Text = nb.ToString();
             this.label4.Text = count.ToString();
             button1.Hide();
             button1.Location = new System.Drawing.Point(286, 289);
-            button1.Show();
-
-            
+            button1.Show();          
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            level = 20;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            level = 15;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            level = 10;
         }
     }
 }
