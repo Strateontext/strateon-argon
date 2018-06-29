a83fbc7c200279f84b2f1f6d52b9cbb517006a06
Strateontext <39223380+Strateontext@users.noreply.github.com>
2018-06-29 22:22:39 +0200

    Update navigator.cpp

--- Strateon/navigator.cpp
+++ Strateon/navigator.cpp
@@ -6,33 +6,13 @@ void FenPrincipale::RenderWindow() {
 
     view = new  QWebEngineView;
     QMdiSubWindow *SubWindowMake = CentralZone->addSubWindow(view);
-
-
-
     SubWindowMake->show();
     SubWindowMake->setWindowTitle("Visualisteur de code");
 
-    UrlEdit = new QLineEdit;
-
-    UrlEdit->show();
-
 }
-
-void FenPrincipale::SetUrl()
-{
-    QUrl UrlSearch = UrlEdit->text();
-    view -> load (UrlSearch);
-}
-
 void FenPrincipale::openhtml()
 {
-
-
     RenderWindow();
     QUrl nomFichier = QFileDialog:: getOpenFileUrl(this, "Ouvrir un fichier", QUrl());
-
-
-        view -> load (nomFichier);
-
-
+    view -> load (nomFichier);
 }
